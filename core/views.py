384721e7870442b82c00b65927497867718854c4
Antoine COULON <a.antoinecoulon@gmail.com>
2025-06-27 18:08:43 +0200

    Feature: tokens

--- core/views.py
+++ core/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Ticket, Message
 from .serializers import TicketSerializer, MessageSerializer
 
 class TicketViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
 
 class MessageViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
