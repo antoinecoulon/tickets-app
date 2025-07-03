58f83c4f29170dfb7098d3394f85fa0280901b53
Antoine COULON <a.antoinecoulon@gmail.com>
2025-07-03 18:29:30 +0200

    Update: greetings

--- user-interface/src/components/Header.tsx
+++ user-interface/src/components/Header.tsx
@@ -1,8 +1,27 @@
+"use client"
+
+import { useUserStore } from "@/store/userStore";
+import dayjs from "dayjs";
+
 export default function Header() {
+  const username = useUserStore((state) => state.username)
+  
+  const greetings = () => {
+    const now = dayjs()
+    const hour = now.hour()
+    console.log('rendering')
+
+    if (hour > 7) {
+      return `Bonjour, ${username} !`
+    } else if (hour > 18) {
+      return `Bonsoir, ${username} !`
+    }
+  }
+  
   return (
     <header className="h-16 bg-white border-b shadow-sm flex items-center px-6 justify-between">
       <div className="text-lg font-semibold">Tableau de bord</div>
-      {/* On affichera ici le rôle / nom de l'utilisateur via Zustand plus tard */}
+      <div>{greetings()}</div>
     </header>
   );
 }
