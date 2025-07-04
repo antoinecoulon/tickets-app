bb17cba34dd3ab020a49d47485051d30249d9e23
Antoine COULON <a.antoinecoulon@gmail.com>
2025-07-04 13:58:38 +0200

    Update: roles conditions

--- user-interface/src/app/dashboard/tickets/[id]/page.tsx
+++ user-interface/src/app/dashboard/tickets/[id]/page.tsx
@@ -9,14 +9,21 @@ import Link from "next/link";
 import { Message } from "@/types/Message";
 import Messages from "./Messages";
 import Badge from "@/components/Badge";
-import { getPrioriteLabel, getPrioriteStyle, getStatutLabel, getStatutStyle } from "@/utils/ticketsBadges";
+import {
+  getPrioriteLabel,
+  getPrioriteStyle,
+  getStatutLabel,
+  getStatutStyle,
+} from "@/utils/ticketsBadges";
+import { useUserStore } from "@/store/userStore";
 
 export default function TicketDetailsPage() {
   const router = useRouter();
   const { id } = useParams();
   const [ticket, setTicket] = useState<Ticket | null>(null);
-  const [messages, setMessages] = useState<Message[]>([])
+  const [messages, setMessages] = useState<Message[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const userRole = useUserStore((state) => state.role);
 
   useEffect(() => {
     const fetchTicket = async () => {
@@ -37,15 +44,15 @@ export default function TicketDetailsPage() {
   useEffect(() => {
     const fetchMessages = async () => {
       try {
-        const response = await api.get(`/tickets/${id}/messages/`)
-        setMessages(response.data.results)
+        const response = await api.get(`/tickets/${id}/messages/`);
+        setMessages(response.data.results);
       } catch (err) {
-        console.error("Erreur lors du chargement des messages: ", err)
+        console.error("Erreur lors du chargement des messages: ", err);
       }
-    }
+    };
 
-    fetchMessages()
-  }, [id])
+    fetchMessages();
+  }, [id]);
 
   if (isLoading) return <div>Chargement en cours...</div>; // TODO: loader
 
@@ -63,35 +70,49 @@ export default function TicketDetailsPage() {
           <strong>Description :</strong> {ticket.description}
         </p>
         <p>
-          <strong>Statut :</strong> <Badge label={getStatutLabel(ticket.statut)} className={getStatutStyle(ticket.statut)} />
+          <strong>Statut :</strong>{" "}
+          <Badge
+            label={getStatutLabel(ticket.statut)}
+            className={getStatutStyle(ticket.statut)}
+          />
         </p>
         <p>
-          <strong>Priorité :</strong> <Badge label={getPrioriteLabel(ticket.priorite)} className={getPrioriteStyle(ticket.priorite)} />
+          <strong>Priorité :</strong>{" "}
+          <Badge
+            label={getPrioriteLabel(ticket.priorite)}
+            className={getPrioriteStyle(ticket.priorite)}
+          />
         </p>
-          <p><strong>Client :</strong> {ticket.client.username}</p>
-          <p><strong>Agent :</strong> {ticket.agent?.username ?? "Non assigné"}</p>
+        <p>
+          <strong>Client :</strong> {ticket.client.username}
+        </p>
+        <p>
+          <strong>Agent :</strong> {ticket.agent?.username ?? "Non assigné"}
+        </p>
         <p>
           <strong>Créé le :</strong>{" "}
-          {dayjs(ticket.createdAt).format("DD/MM/YYYY à HH:MM")}
+          {dayjs(ticket.createdAt).format("DD/MM/YYYY à HH:mm")}
         </p>
-
-        <div className="mt-6">
-          <Link
-            href={`/dashboard/tickets/${id}/edit`}
-            className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
-          >
-            Modifier ce ticket
-          </Link>
-        </div>
-      </div>
-        
-          <Messages 
-            messages={messages}
-            setMessages={setMessages}
-            id={Number(id)}
-          />
-          
-          
+        {(userRole === "admin" || userRole === "agent") && (
+          <div className="mt-6">
+            <Link
+              href={`/dashboard/tickets/${id}/edit`}
+              className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
+            >
+              Modifier ce ticket
+            </Link>
           </div>
+        )}
+        {userRole === "client" &&
+          <div className="mt-4 text-sm text-gray-500 italic">
+            <p>
+              Vous ne pouvez pas modifier ce ticket, contactez un agent si nécessaire.
+            </p>
+          </div>
+        }
+      </div>
+
+      <Messages messages={messages} setMessages={setMessages} id={Number(id)} />
+    </div>
   );
 }
