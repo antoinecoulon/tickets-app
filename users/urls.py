74ec9be497261d9b7b09ed36dfce941bce2adf7a
Antoine COULON <a.antoinecoulon@gmail.com>
2025-07-06 21:44:51 +0200

    Feature: stats

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import current_user, agents_list
+from .views import current_user, agents_list, AdminStatsView
 
 urlpatterns = [
     path('auth/credentials/', current_user),
-    path('auth/agents/', agents_list)
+    path('auth/agents/', agents_list),
+    path('admin/stats/', AdminStatsView.as_view(), name='admin-stats')
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,36 @@
 from django.contrib.auth import get_user_model
+from rest_framework.views import APIView
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from core.permissions import IsAdmin
 from rest_framework.response import Response
+from core.models import Ticket, Entreprise
 
 User = get_user_model()
+
+class AdminStatsView(APIView):
+    permission_classes = [IsAuthenticated, IsAdmin]
+
+    def get(self, request):
+        tickets_total = Ticket.objects.count()
+        agents_total = User.objects.filter(role='agent').count()
+        clients_total = User.objects.filter(role='client').count()
+        entreprises_total = Entreprise.objects.count()
+
+        tickets_by_statut = {
+            "ouvert": Ticket.objects.filter(statut="ouvert").count(),
+            "en_cours": Ticket.objects.filter(statut="en_cours").count(),
+            "resolu": Ticket.objects.filter(statut="resolu").count(),
+            "ferme": Ticket.objects.filter(statut="ferme").count(),
+        }
+
+        return Response({
+            "tickets_total": tickets_total,
+            "agents_total": agents_total,
+            "clients_total": clients_total,
+            "entreprises_total": entreprises_total,
+            "tickets_by_statut": tickets_by_statut
+        })
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
